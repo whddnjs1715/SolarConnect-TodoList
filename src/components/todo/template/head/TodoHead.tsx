85b70e02c69f81717eb2165da20406b22d63846e
whddnjs1715 <whddnjs1715@naver.com>
2021-08-20 22:36:24 +0900

    init: first init #1

---
+++ src/components/todo/template/head/TodoHead.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+const TodoHeadBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 52px;
+  padding-bottom: 24px;
+  border-bottom: 3px solid #33bb77;
+`;
+
+const DateText = styled.div`
+  font-size: 26px;
+  color: #119955;
+  padding-left: 10px;
+`;
+
+const DayText = styled.div`
+  font-size: 22px;
+  color: #119955;
+  padding-top: 5px;
+`;
+
+const TodoHead = () => {
+  //@TODO 현재 시간을 표시해야합니다.
+  const dayString = "Tuesday";
+  const dateString = "July 20, 2021";
+
+  return (
+    <TodoHeadBlock>
+      <DayText>{dayString}</DayText>
+      <DateText>{dateString}</DateText>
+    </TodoHeadBlock>
+  );
+};
+
+export default React.memo(TodoHead);
